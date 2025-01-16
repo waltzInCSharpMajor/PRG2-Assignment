1b3931ec81eca5253e6505b384c57ccfdba84b5c
mr.thehackerman <s10268118@connect.np.edu.sg>
2025-01-16 11:56:57 +0800

    Add variables to Terminal class (incomplete)

--- code/code/Terminal.cs
+++ code/code/Terminal.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace code
 {
-    internal class Terminal
+    public class Terminal
     {
+        private string _terminalName;
+
+        public string TerminalName
+        {
+            get { return _terminalName; }
+            set { _terminalName = value; }
+        }
+
+        //private Dictionary<string, Airline> _airlines;
+
+        //public Dictionary<string, Airline> Airlines
+        //{
+        //    get { return _airlines; }
+        //    set { _airlines = value; }
+        //}
+
+        private Dictionary<string, Flight> _flights;
+
+        public Dictionary<string, Flight> Flights
+        {
+            get { return _flights; }
+            set { _flights = value; }
+        }
+
+        //private Dictionary<string, BoardingGate> _boardingGates;
+
+        //public Dictionary<string, BoardingGate> BoardingGates
+        //{
+        //    get { return _boardingGates; }
+        //    set { _boardingGates = value; }
+        //}
+
+        private Dictionary<string, double> _gateFees;
+
+        public Dictionary<string, double> GateFees
+        {
+            get { return _gateFees; }
+            set { _gateFees = value; }
+        }
+
+
     }
 }
